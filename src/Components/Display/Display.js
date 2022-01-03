1772d35ba8b63557e78135dc88341fadaa22f2c7
SamerSiam <ssiam05@gmail.com>
2022-01-04 01:02:38 +0200

    bug fixing

--- src/Components/Display/Display.js
+++ src/Components/Display/Display.js
@@ -1,71 +1,63 @@
-import React, {useState,useEffect} from 'react'
-import './Display.css'
-import coinData from '../../API/Coin.api'
-import Spinner from '../Spinner/Spinner'
+import React, { useState, useEffect } from "react";
+import "./Display.css";
+import coinData from "../../API/Coin.api";
+import Spinner from "../Spinner/Spinner";
 
 function Display() {
-   
-  const [results, setResults]=useState([]);
-  const [isLoading, setLoading]=useState(false); 
+  const [results, setResults] = useState([]);
+  const [isLoading, setLoading] = useState(false);
   const [errorMsg, setErrorMsg] = useState("Loading");
 
-  useEffect(()=>{
-   
-    const fetch=async()=>{
-    setErrorMsg("");
-    setLoading(true)
-try{ 
-    const {data}=await coinData.get(); 
-    setResults(data) ;
-    setLoading(false);  
-    console.log("data is",data) 
-}
-catch (err) {setErrorMsg(err.message)  }
-    
-    
-};
-fetch();
+  useEffect(() => {
+    const fetch = async () => {
+      setErrorMsg("");
+      setLoading(true);
+      try {
+        const { data } = await coinData.get();
+        setResults(data);
+        setLoading(false);
+        console.log("data is", data);
+      } catch (err) {
+        setErrorMsg(err.message);
+      }
+    };
+    fetch();
+  }, []);
 
-}, []); 
-
-    //  {/* {isLoading? <Spinner message={errorMsg}/>
-    //     :<Display items={results}/>}  */}
-    
-    if (isLoading===true)
-    {
-      return <Spinner message={errorMsg}/>
-    }
-  else{
+  if (isLoading === true) {
+    return <Spinner message={errorMsg} />;
+  } else {
     return (
       <div className="display-container">
         <table className="ui fixed table">
-      <thead>
-      <tr>
-        <th>Name</th>
-        <th>Symbol</th>
-        <th>Name</th>
-        <th>Logo</th>
-        <th>Current Price</th>
-      </tr>
-      </thead>
-      <tbody>
-        {results.map((item, index) => (
-      <div key={item.id} >
-      <tr>
-        <td className="name"> {item.name}</td>
-        <td className="symbol"> {item.symbol}</td>
-        <td className="logo"> <img src={item.image} alt={item.name}/> </td>
-        <td> {item.current_price}</td>
-        </tr>
-        </div>
-    ))} 
-      </tbody>
-     
-    </table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Symbol</th>
+              <th>Name</th>
+              <th>Logo</th>
+              <th>Current Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {results.map((item, index) => (
+              <div key={item.id}>
+                <tr>
+                  <td className="name"> {item.name}</td>
+                  <td className="symbol"> {item.symbol}</td>
+                  <td className="logo">
+                    {" "}
+                    <img src={item.image} alt={item.name} />{" "}
+                  </td>
+                  <td> {item.current_price}</td>
+                </tr>
+              </div>
+            ))}
+          </tbody>
+        </table>
       </div>
-  )
-  }}
+    );
+  }
+}
 
-    
-
-export default Display
+export default Display;
